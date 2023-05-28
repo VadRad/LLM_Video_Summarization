ac214699403e657ae2771b5893d914e2a6cecded
Vadim Radchenko <vadik1000@gmail.com>
2023-05-28 19:25:49 +0300

    clear repo

--- video_asr.py
+++
@@ -1,9 +0,0 @@
-import whisper
-
-class TextExtractor:
-    def __init__(self, model_path):
-        self.model = whisper.load_model(model_path)
-    
-    def extract_text_from_audio(self, audio_path):
-        result = self.model.transcribe(audio_path)
-        return result["text"]
